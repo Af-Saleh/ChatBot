4bfd9f6abae7ba54ab14225a4e0e59fa390cfc7f
Af-Saleh <salehaf19.5@gmail.com>
2026-08-18 20:48:27 +0100

    Update full_code.py

--- full_code.py
+++ full_code.py
@@ -18,7 +18,7 @@ class messagebox:
         self.send_command = send_command
     def initialize_shapes(self):
         self.frame = CTkFrame(self.master , corner_radius=25 , border_color='black' , border_width=2 , fg_color='white')
-        self.textbox = Text(self.frame, height =1, border=0, font=('Noto Sans' , 17) , wrap='word',width = 60)
+        self.textbox = Text(self.frame, height =1, border=0, font=('Noto Sans' , 17) , wrap='word',width = 40)
         self.scrlbar = CTkScrollbar(self.frame,command=self.textbox.yview)
         self.textbox.configure(yscrollcommand=self.scrlbar.set)
         self.canvas = Canvas(self.frame , width=39 , height=39 , bg='white' ,  borderwidth = 0 , highlightthickness=0)
@@ -29,6 +29,9 @@ class messagebox:
         self.frame.pack(pady = 15 , side = 'bottom' , padx = 10 )
         self.textbox.pack(pady =(9,8) , padx=(13,5), side='left')
         self.canvas.pack(pady = (4,5), padx=(0,11), side='bottom',anchor='e')
+        self.frame.update()
+        self.frame.pack_forget()
+        self.frame.place(x=(self.master.winfo_width()-self.frame.winfo_width()-40)/2 , y=self.master.winfo_screenheight()-135)
     def hover(self , e=None):
         if e.type == '7' :
             self.canvas.itemconfigure(self.oval, fill = "#6AC5EE")
@@ -52,6 +55,7 @@ class messagebox:
         self.canvas.itemconfigure(self.oval, fill="#6AC5EE")
     def update_height(self):
         lines = self.textbox.count('1.0' , 'end-1c' , 'displaylines' , return_ints=True)
+        self.frame.place_configure(x=(self.master.winfo_width()-self.frame.winfo_width()-40)/2 , y=self.master.winfo_screenheight()-135-(min(lines , 8)*26))
         self.textbox['height'] = min(lines+1 , 9)
         if lines+1 > 9 :
             self.scrlbar.pack(side = 'top' , anchor = 'e' , padx = (0,17) , pady = (2,5))
@@ -104,14 +108,15 @@ class header:
     def font(self , size):
         return 'Cascadia Code' , size , 'bold'
     def initialize(self):
-        self.frame = CTkFrame(self.master , fg_color=self.color , corner_radius=10)
+        self.frame = CTkFrame(self.master , fg_color=self.color , corner_radius=10 , width=1000 , height=90)
         self.avatar = CTkCanvas(self.frame , width=70 , height=70 , bg=self.color , borderwidth = 0 , highlightthickness=0)
         self.textframe = CTkFrame(self.frame , fg_color=self.color)
         shrink = 9
         self.photo = self.photo.subsample(shrink,shrink)
         self.avatar.create_image(35, 35,image=self.photo)
     def placeall(self):
-        self.frame.pack(side = 'top' , fill = 'x' , padx = 5 , pady = 5)
+        self.frame.pack( padx = 5 , pady = 5)
+        self.frame.pack_propagate(False)
         self.avatar.pack(side='left' , padx=10 , pady=10)
         self.textframe.pack(side='left' , padx=5)
         CTkLabel(self.textframe , text='Local AI' , font=self.font(17) , fg_color=self.color , text_color='white').pack(padx = 1 , pady=5)
@@ -119,12 +124,23 @@ class header:
     def main(self):
         self.initialize()
         self.placeall()
+class chat_display:
+    def __init__(self , master):
+        self.user_message , self.AIreply , self.frame = [None]*3
+        self.master = master
+    def initialize(self):
+        pass
+        
 
 def printmessage(message):
     print(message)
 set_appearance_mode('light')
-window = Tk()
-h = header(window , "#0B1F3A")
+win = Tk()
+frame = CTkFrame(win , border_color='black' , border_width=2)
+frame.pack(fill='y' , side = 'right' , padx = 5)
+win.state('zoomed')
+win.update()
+h = header(frame , "#0B1F3A")
 h.main()
-messagebox(window , printmessage).main()
-window.mainloop()
+messagebox(frame , printmessage).main()
+win.mainloop()
